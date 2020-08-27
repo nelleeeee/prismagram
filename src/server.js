fa010e4a9f4828e89f280dd7252c979c696f1835
nelleeeee <rudghksldl@gmail.com>
2020-08-28 00:36:49 +0900

    #1.2 Setting Up the Server like the Pros

--- src/server.js
+++ src/server.js
@@ -1,22 +1,14 @@
-require("dotenv").config();
-import { GraphQLServer } from "graphql-yoga";
+require("dotenv").config();  // 환경변수 설정 - 포트 넘버 설정
+import { GraphQLServer } from "graphql-yoga"; // graphql 서버(express 서버 내장. 미들웨어 사용은 express로)
+import logger from "morgan"; // express 미들웨어 로거
+import schema from "./schema";
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-    type Query{
-        hello: String!
-    }    
-`;
-
-const resolvers = {
-    Query: {
-        hello: () => "Hi"
-    }
-};
-
 const server = new GraphQLServer({
-    typeDefs, resolvers
+    schema// 스키마, 리졸버
 });
 
-server.start({ port: PORT }, () => console.log(`Server running on port ${PORT}`));
+server.express.use(logger("dev")); //미들웨어 로거
+
+server.start({ port: PORT }, () => console.log(`😁 Server running on port ${PORT}`));
