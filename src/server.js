f99b8fcd9805da641819768fcd8e0594b20a28ee
nelleeeee <rudghksldl@gmail.com>
2020-08-29 11:57:22 +0900

    toggleLike

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
-require("dotenv").config();  // 환경변수 설정 - 포트 넘버 설정
+import "./env";
 import { GraphQLServer } from "graphql-yoga"; // graphql 서버(express 서버 내장. 미들웨어 사용은 express로)
 import logger from "morgan"; // express 미들웨어 로거
 import schema from "./schema";
+import "./passport";
+import { authenticateJwt } from "./passport";
 
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({
-    schema// 스키마, 리졸버
+  schema,
+  context: ({ request }) => ({ request }), // 스키마, 리졸버
 });
 
 server.express.use(logger("dev")); //미들웨어 로거
+server.express.use(authenticateJwt);
 
-server.start({ port: PORT }, () => console.log(`😁 Server running on port ${PORT}`));
+server.start({ port: PORT }, () =>
+  console.log(`😁 Server running on port ${PORT}`)
+);
