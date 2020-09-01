99b53e8c8689b93b85c9f562f83df9cb17a4a0a2
nelleeeee <rudghksldl@gmail.com>
2020-09-02 00:28:02 +0900

    fragment 수정

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,14 +4,18 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
-      const user = await prisma.createUser({
+      const exists = await prisma.$exists.user({ username });
+      if (exists) {
+        throw Error("이미 있는 유저이름");
+      }
+      await prisma.createUser({
         username,
         email,
         firstName,
         lastName,
         bio,
       });
-      return user;
+      return true;
     },
   },
 };
